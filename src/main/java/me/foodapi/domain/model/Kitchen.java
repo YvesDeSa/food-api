bcac756b7f6c716f4deb5cdab2c72fdcc6b384ef
Yves de Sá <yvesdsb98@gmail.com>
2022-06-28 18:45:57 -0300

    feat: created persistence classes for kitchen and restaurant

---
+++ src/main/java/me/foodapi/domain/model/Kitchen.java
@@ -0,0 +1,45 @@
+package me.foodapi.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_kitchen")
+@Getter
+@Setter
+public class Kitchen {
+  @Id
+  private Long id;
+
+  private String name;
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Kitchen other = (Kitchen) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+}
